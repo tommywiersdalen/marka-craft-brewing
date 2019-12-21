5cc296251658ca8d1c14667687e82aaa0db77ec6
Tommy <tommy@getacademy.no>
2019-12-21 23:48:12 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,13 @@
 </template>
 
 <script>
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
 
 <style>
